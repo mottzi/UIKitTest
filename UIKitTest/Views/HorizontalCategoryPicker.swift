a168ab7d11641765b4c7ae4c48f91de2e3857008
Berken Sayilir <sayilir.berken@gmail.com>
2024-10-22 12:13:08 +0200

    - scrollview working - onSelection button reordering (onDeselection missing)

--- UIKitTest/Views/HorizontalCategoryPicker.swift
+++ UIKitTest/Views/HorizontalCategoryPicker.swift
@@ -2,26 +2,48 @@ import UIKit
 
 class HorizontalCategoryPicker: UIViewController
 {
-    override func viewDidLoad() 
+    let scrollview = UIScrollView()
+    let stack = UIStackView()
+
+    override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        let stack = UIStackView()
-        stack.axis = .horizontal
-        stack.spacing = 6
+        self.view.backgroundColor = .systemBackground
         
-        stack.addArrangedSubview(CustomButton(title: "Category", icon: "bolt.fill"))
-        stack.addArrangedSubview(CustomButton(title: "Category", icon: "tree"))
-
+        scrollview.showsHorizontalScrollIndicator = false
+        scrollview.translatesAutoresizingMaskIntoConstraints = false
+        
+        stack.axis = .horizontal
+        stack.spacing = 8
         stack.translatesAutoresizingMaskIntoConstraints = false
+        
+        for category in MapCategory.allCategories
+        {
+            stack.addArrangedSubview(CustomButton(title: category.title, icon: category.icon))
+        }
+        
+        scrollview.addSubview(stack)
 
-        self.view.addSubview(stack)
+        self.view.addSubview(scrollview)
 
-        NSLayoutConstraint.activate([
-            stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-        ])
+        NSLayoutConstraint.activate(controlConstraints)
     }
+    
+    lazy var controlConstraints: [NSLayoutConstraint] =
+    [
+        // scroll view constraints
+        scrollview.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+        scrollview.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+        scrollview.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+        scrollview.heightAnchor.constraint(equalTo: stack.heightAnchor),
+        
+        // stack view constraints
+        stack.leadingAnchor.constraint(equalTo: scrollview.leadingAnchor, constant: 12),
+        stack.trailingAnchor.constraint(equalTo: scrollview.trailingAnchor, constant: -12),
+        stack.topAnchor.constraint(equalTo: scrollview.topAnchor),
+        stack.bottomAnchor.constraint(equalTo: scrollview.bottomAnchor),
+    ]
 }
 
 class CustomButton: UIButton
@@ -40,27 +62,79 @@ class CustomButton: UIButton
     }
         
     private func setupButton()
-    {        
+    {
+        // shape
         self.configuration = .filled()
         self.configuration?.cornerStyle = .capsule
+        self.configuration?.baseForegroundColor = .darkText
+        self.configuration?.baseBackgroundColor = UIColor(named: self.isSelected ? "ButtonSelected" : "ButtonUnselected")
+        
+        // title
         self.configuration?.attributedTitle = AttributedString(self.title, attributes: AttributeContainer([.font: UIFont.boldSystemFont(ofSize: 16)]))
+        
+        // icon
         self.configuration?.image = UIImage(systemName: self.icon)
         self.configuration?.imagePadding = 4
         self.configuration?.preferredSymbolConfigurationForImage = .init(font: .boldSystemFont(ofSize: 14))
-        self.configuration?.baseForegroundColor = .darkText
-        self.configuration?.baseBackgroundColor = self.isSelected ? .systemBlue.withAlphaComponent(0.8) : .systemBlue.withAlphaComponent(0.3)
-
-        self.addTarget(self, action: #selector(onButtonTap), for: .touchUpInside)
+        
+        // width based on content
+        self.setContentHuggingPriority(.required, for: .horizontal)
+        self.setContentCompressionResistancePriority(.required, for: .horizontal)
+        
+        // button handler
+        self.addAction(UIAction { [weak self] _ in self?.toggleButton() }, for: .touchUpInside)
     }
-    
-    @objc func onButtonTap()
+        
+    private func toggleButton()
     {
         self.isSelected.toggle()
         
-        self.configuration?.baseBackgroundColor = self.isSelected ? .systemBlue.withAlphaComponent(0.8) : .systemBlue.withAlphaComponent(0.3)
+        self.configuration?.baseBackgroundColor = UIColor(named: self.isSelected ? "ButtonSelected" : "ButtonUnselected")
+
+        if self.isSelected
+        {
+            if let stack = self.superview as? UIStackView
+            {
+                var index = 0
+                for view in stack.arrangedSubviews
+                {
+                    if let button = view as? CustomButton, button.isSelected, button != self
+                    {
+                        index += 1
+                    }
+                }
+                
+                UIView.animate(withDuration: 0.33, delay: 0, options: .curveEaseInOut)
+                {
+                    stack.removeArrangedSubview(self)
+                    self.removeFromSuperview()
+                    
+                    stack.insertArrangedSubview(self, at: index)
+                    
+                    stack.layoutIfNeeded()
+                }
+            }
+        }
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
+
+struct MapCategory
+{
+    let title: String
+    let icon: String
+        
+    static let allCategories: [MapCategory] =
+    [
+        MapCategory(title: "Movies", icon: "film"),
+        MapCategory(title: "Park", icon: "leaf"),
+        MapCategory(title: "Eat", icon: "fork.knife"),
+        MapCategory(title: "Sport", icon: "sportscourt"),
+        MapCategory(title: "Museum", icon: "building.columns"),
+        MapCategory(title: "Zoo", icon: "tortoise"),
+        MapCategory(title: "Amusement", icon: "sparkles")
+    ]
 }
 
 #Preview
---
+++ UIKitTest/Views/legacy.swift
@@ -0,0 +1,17 @@
+//override func viewDidLayoutSubviews()
+//{
+//    super.viewDidLayoutSubviews()
+//    
+//    // Ensure layout is updated
+//    stack.layoutIfNeeded()
+//    
+//    var counter = 1
+//    for button in stack.arrangedSubviews
+//    {
+//        guard let button = button as? CustomButton else { continue }
+//        print("Button\(counter).width: \(button.frame.width)")
+//        counter += 1
+//    }
+//}
+
+//self.addTarget(self, action: #selector(onButtonTap), for: .touchUpInside)
