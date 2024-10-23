c93887df39d92cf786b81a35a3a7f0e3cf94de9e
Berken Sayilir <sayilir.berken@gmail.com>
2024-10-24 00:06:56 +0200

    - added .debug() - fixed font and weights - fixed constraints

---
+++ UIKitTest/Abstract/UIFont.swift
@@ -0,0 +1,8 @@
+//
+//  UIFont.swift
+//  UIKitTest
+//
+//  Created by Berken Sayilir on 23.10.2024.
+//
+
+import Foundation
---
+++ UIKitTest/Abstract/UIView.swift
@@ -0,0 +1,8 @@
+//
+//  UIView.swift
+//  UIKitTest
+//
+//  Created by Berken Sayilir on 23.10.2024.
+//
+
+import Foundation
